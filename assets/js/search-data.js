cc16970a85ea0dcde5535eeecdfce4ac11e79290
wuyuqing0327 <wuyuqing0327@gmail.com>
2026-02-20 22:52:31 +0000

    Deploying to gh-pages from @ wuyuqing0327/wuyuqing0327.github.io@f5ef4383b49b88cde33c744e7c1ebc3ee5fbf859 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -486,13 +486,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/chelsea-uchi0327", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
